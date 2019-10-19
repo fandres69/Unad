900d1f6584273a3e39e80bbbf7098ad166c54ca3
Fandres <fandres69@gmail.com>
2019-10-19 17:26:33 -0500

    Modelo Latout finalizado

--- wwwroot/Js/OVI.js
+++ wwwroot/Js/OVI.js
@@ -1,8 +1,8 @@
 /*
-* Autor: Fabio Andres Tobon Quesada
+* Autor: Fabio Andres Tobón Quesada
 * email: fandres69@gmail.com
 * Fecha: Octubre de 2019
-* Descripcion: Hoja de estilos pagina modelo OVI conecxion a Mysql con Php.
+* Descripción: Hoja de estilos pagina modelo OVI conexión a Mysql con Php.
 *
 */
 
@@ -10,10 +10,16 @@
 function openNav() {
     document.getElementById("Lateral").style.width = "250px";
     document.getElementById("Contenido").style.marginLeft = "250px";
+    document.getElementById('btnabrir').style.display="none";
+    document.getElementById('btncerrar').style.display="block";
   }
   
  
   function closeNav() {
     document.getElementById("Lateral").style.width = "0";
     document.getElementById("Contenido").style.marginLeft = "0";
+    document.getElementById('btncerrar').style.display="none";
+    document.getElementById('btnabrir').style.display="block";
   }
+
+  
